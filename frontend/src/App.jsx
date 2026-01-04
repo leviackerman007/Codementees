2088a4e4237446780596a8e98b2c8bacafd5dccd
leviackerman007 <kgs.tushar522@gmail.com>
2026-01-04 20:22:58 +0000

    Few corrections made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Privacy from "./pages/Privacy";
+import Terms from "./pages/Terms";
+import Contact from "./pages/Contact";
 
 export default function App() {
   return (
@@ -8,6 +11,9 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 
+
 export default function Footer() {
     return (
         <footer className='bg-gray-900 text-gray-300'>
